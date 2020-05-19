7019190a306155221d5dcf77ef8824c3b39fc3bd
zhuoyi <776870907@qq.com>
2020-05-19 16:00:22 +0800

    test6

--- src/pages/kylin/views/back.vue
+++ src/pages/kylin/views/back.vue
@@ -8,7 +8,7 @@
 import { Component, Vue } from "vue-property-decorator";
 import Button from "@mandmobile/button"; //MARK:文件路径:使用源码版本组件
 import Toast from "@mandmobile/toast"; //MARK:文件路径:使用源码版本组件
-
+   
 @Component({
   components: {
     [Button.name]: Button
